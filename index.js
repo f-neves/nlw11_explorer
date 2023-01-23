b5f67a3e65620fb7f659592ad16a6f5a745d252c
Felipe <neves.mecanica@gmail.com>
2023-01-23 04:32:07 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+let today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+function add() {
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Day already exists!")
+    return
+  } else {
+    nlwSetup.addDay(today)
+    alert("Day added!")
+    return
+  }
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
